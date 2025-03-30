68a6ec048d1d3e6d42c085778afdcf9b8851f442
Braian <101738428+BraianC12@users.noreply.github.com>
2025-03-30 13:02:10 -0300

    Add files via upload

---
+++ Complejidad temporal/ActividadesEntregar/unidad01/Ejercicio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace unidad01
+{
+	public class Ejercicio
+	{		
+		public ArbolBinario<int> Nuevo(ArbolBinario<int> arbol)
+        {
+			if(arbol ==null){
+				return null;
+			}
+			
+			ArbolBinario<int> arbolNuevo = new ArbolBinario<int>(arbol.valor);
+
+		    if (arbol.izquierdo != null)
+		    {
+		        arbolNuevo.izquierdo = new ArbolBinario<int>(arbol.izquierdo.valor + arbol.valor);
+				arbolNuevo.izquierdo.izquierdo = new ArbolBinario<int>(arbol.izquierdo.izquierdo.valor + arbol.izquierdo.valor);		        
+		        arbolNuevo.izquierdo.derecho = Nuevo(arbol.izquierdo.derecho);
+		    }
+		    
+		    arbolNuevo.derecho = Nuevo(arbol.derecho);
+		
+		    return arbolNuevo;
+        }
+	}
+}
