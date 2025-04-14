e3f3064cc219897f4f33221cb9c427a3e1f137e4
Braian <braianenrique89@gmail.com>
2025-04-14 16:10:11 -0300

    Realizacion del tp3 y organizacion de archivos

---
+++ Ejercicios/Tp2/MyClass.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: HERIC
+ * Fecha: 13/4/2025
+ * Hora: 20:01
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Collections.Generic;
+
+namespace Tp2
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		
+	}
+}
---
+++ Ejercicios/Tp3/MyClass.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: HERIC
+ * Fecha: 13/4/2025
+ * Hora: 20:02
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Collections.Generic;
+
+namespace Tp3
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		
+	}
+}
