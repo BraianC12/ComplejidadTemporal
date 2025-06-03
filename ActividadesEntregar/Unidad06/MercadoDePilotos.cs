87c6f2c3488fde6b0412a9ba0a0df54885fe4b6b
Braian <braianenrique89@gmail.com>
2025-06-03 20:11:33 -0300

    Agrego la primera version de la practica 06

---
+++ ActividadesEntregar/Unidad06/MercadoDePilotos.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Unidad06
+{
+	/// <summary>
+	/// Description of MercadoDePilotos.
+	/// </summary>
+	   public class MercadoDePilotos
+	   {
+	   	private List<string>mejor;
+	   	
+	   	public List<string> PilotoQuePasoPorMasEscuderias(Grafo<string> escuderias){
+	   		mejor = new List<string>();
+	
+	        Vertice<string> origen = null;
+	        
+	        foreach (var v in escuderias.getVertices())
+	        {
+	            if (v.getDato() == "origen")
+	            {
+	                origen = v;
+	                break;
+	            }
+	        }
+	
+	        if (origen == null)
+	            return mejor;
+	
+	        // Por cada piloto desde origen
+	        foreach (var a in origen.getAdyacentes())
+	        {
+	            List<string> nuevoCamino = new List<string> { origen.getDato() };
+	            DFS(a.getDestino(), a.getPeso(), nuevoCamino);
+	        }
+	
+	        return mejor;
+	    }
+	
+	    private void DFS(Vertice<string> actual, int piloto, List<string> camino)
+	    {
+	        camino.Add(actual.getDato());
+	
+	        if (camino.Count > mejor.Count)
+	        {
+	            mejor = new List<string>(camino);
+	        }
+	
+	        foreach (var a in actual.getAdyacentes())
+	        {
+	            if (a.getPeso() == piloto)
+	            {
+	                DFS(a.getDestino(), piloto, camino);
+	            }
+	        }
+	
+	        camino.RemoveAt(camino.Count - 1);
+	    }
+	   	
+        }
+    }
+
---
+++ ActividadesEntregar/Unidad06/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Unidad06
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Grafo<string> grafo=new Grafo<string>();
+			
+			Vertice<string> origen=new Vertice<string>("origen");
+			Vertice<string> escuderia1=new Vertice<string>("Ferrari");
+			Vertice<string> escuderia2=new Vertice<string>("McLaren");
+			Vertice<string> escuderia3=new Vertice<string>("Mercedes");
+			
+			
+			grafo.agregarVertice(origen);
+			grafo.agregarVertice(escuderia1);
+			grafo.agregarVertice(escuderia2);
+			grafo.agregarVertice(escuderia3);
+			
+			origen.getAdyacentes().Add(new Arista<string>(escuderia1, 1));
+			escuderia1.getAdyacentes().Add(new Arista<string>(escuderia2, 1));
+			escuderia2.getAdyacentes().Add(new Arista<string>(escuderia3, 1));
+			escuderia3.getAdyacentes().Add(new Arista<string>(origen, 2));
+			
+			MercadoDePilotos mercado=new MercadoDePilotos();
+			List<string> caminoMasLargo=mercado.PilotoQuePasoPorMasEscuderias(grafo);
+  
+            foreach (var e in caminoMasLargo){
+                Console.WriteLine(e);
+            }
+            
+			Console.ReadKey(true);
+		}
+	}
+}
