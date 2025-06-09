36c3ae2ad1af83f56dff7c16c6cb7807755d7100
Braian <braianenrique89@gmail.com>
2025-06-08 22:59:43 -0300

    Ultimos dos puntos del TP Final

--- Trabajo final/TPF_WIW_Sharp/WiW_Sharp/WiW/Estrategia.cs
+++ Trabajo final/TPF_WIW_Sharp/WiW_Sharp/WiW/Estrategia.cs
@@ -28,14 +28,46 @@ namespace tpfinal
 
 		public String Consulta2(ArbolBinario<DecisionData> arbol)
 		{
+			if(arbol.esHoja()){
+				return "Prediccion: " + arbol.getDatoRaiz() + "\n";
+			}
+			else{
+				string caminoIzquierdo=arbol.getDatoRaiz().ToString() + " -> Si ->" + Consulta2(arbol.getHijoIzquierdo());
+				string caminoDerecho=arbol.getDatoRaiz().ToString() + " -> no ->" + Consulta2(arbol.getHijoDerecho());
+				return caminoIzquierdo + caminoDerecho;
+			}
 
-			return "Implementar";
+			
 		}
 
 
 		public String Consulta3(ArbolBinario<DecisionData> arbol)
 		{
-			string result = "Implementar";
+			Queue<ArbolBinario<DecisionData>> cola=new Queue<ArbolBinario<DecisionData>>();
+			
+			int nivel=0;
+			
+			cola.Enqueue(arbol);
+			string result = "";
+			
+			while(cola.Count !=0){
+				int tamañoNivel=cola.Count;
+				result=result + "Nivel " + nivel + ":\n";
+				
+				for(int i=0; i<tamañoNivel; i++){
+					ArbolBinario<DecisionData> nodoActual=cola.Dequeue();
+					result=result + nodoActual.getDatoRaiz() + "\n";
+					
+					if(nodoActual.getHijoIzquierdo() !=null){
+						cola.Enqueue(nodoActual.getHijoIzquierdo());
+					}
+					if(nodoActual.getHijoDerecho() !=null){
+						cola.Enqueue(nodoActual.getHijoDerecho());
+					}
+				}
+				
+				nivel++;				
+			}
 			return result;
 		}
 
